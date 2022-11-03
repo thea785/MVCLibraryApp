8cc7784af634f10d87fdd9627881e35f385a4079
thea785 <theawallnc@gmail.com>
2022-11-03 09:56:14 -0400

    Created Hashing unit test

---
+++ LibraryUnitTest/HashingTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryCommon;
+using System;
+
+namespace LibraryUnitTest
+{
+    [TestClass]
+    public class HashingTest
+    {
+        [TestMethod]
+        public void TestHashing()
+        {
+            string hash, salt;
+            string password = "asdf";
+
+            Hashing.GenerateSaltedHash(password, out hash, out salt);
+
+            //Console.WriteLine(hash);
+            //Console.WriteLine(salt);
+
+            Assert.IsTrue(Hashing.VerifyPassword("asdf", hash, salt));
+        }
+    }
+}
