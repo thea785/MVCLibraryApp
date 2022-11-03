56006923897c8abef8a9f4d681c3a0b4660074b4
thea785 <theawallnc@gmail.com>
2022-11-03 14:49:07 -0400

    Added common classes

---
+++ LibraryCommon/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryCommon
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int CheckedOutBy { get; set; }
+        public int OnHoldBy { get; set; }
+    }
+}
---
+++ LibraryCommon/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryCommon
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public int RoleID { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HashedPassword { get; set; }
+        public string Salt { get; set; }
+    }
+}
