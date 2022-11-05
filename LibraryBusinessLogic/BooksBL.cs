024448d9e26ce8199cf581aa51795af19ca4bbaf
thea785 <theawallnc@gmail.com>
2022-11-05 14:18:41 -0400

    #1 Created BooksBL class

---
+++ LibraryBusinessLogic/BooksBL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LibraryCommon;
+using LibraryData;
+
+namespace LibraryBusinessLogic
+{
+    public static class BooksBL
+    {
+        public static List<Book> GetBooks()
+        {
+            return BooksData.GetBooks();
+        }
+
+        public static void CheckoutBook(int bookID, int userID)
+        {
+            BooksData.CheckoutBook(bookID, userID);
+        }
+
+        public static void ReturnBook(int bookID)
+        {
+            BooksData.ReturnBook(bookID);
+        }
+
+        public static void HoldBook(int bookID, int userID)
+        {
+            BooksData.HoldBook(bookID, userID);
+        }
+    }
+}
