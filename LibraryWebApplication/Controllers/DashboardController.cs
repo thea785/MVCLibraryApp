ac448a74a66efb41427395b297f013a6445ba1da
thea785 <theawallnc@gmail.com>
2022-11-08 09:47:51 -0500

    #5 #6 Created dashboard outline view

---
+++ LibraryWebApplication/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryWebApplication.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("/Views/Dashboard/Dashboard.cshtml");
+        }
+    }
+}
