e2053c8c65faad3b1e111a450758cb5d980d192e
thea785 <theawallnc@gmail.com>
2022-11-09 10:41:20 -0500

    Added user delete unit test

--- LibraryUnitTest/UsersDataTest.cs
+++ LibraryUnitTest/UsersDataTest.cs
@@ -9,7 +9,7 @@ namespace LibraryUnitTest
     public class UsersDataTest
     {
         [TestMethod]
-        public void CreateUserTest()
+        public void CreateAndDeleteUserTest()
         {
             // Create a user in the database
             int userID = UsersData.CreateUser(2, "test1@gmail.com", "1Fname", "1Lname", "SM3wWClN1KFFBCm9GLMs9m6lJ4yzNQ8zVfAntSym1Xty1wMBDZmA9OiE1IB9ovDruRP8fW44hrpQ8Ca6W9JmQPhl0MA4uDsluQVrhQyMN0FexhgKvH9eCAqz4G88GfM5gqS+G/M6myVUlzKi6KwWc/B73IcJiPN2tnV8+neY0wkAMLz/yIJ6b8vqOYoziF+vWRsXnSDOw1Q/PKCcnKZYtFa7B2NI1dFW/uOHo8HpM0rNEyMOKsvHp0lv1rIuA8VIsamJ/UJ5hmNwdSvE5Vd4428KjDTK2S23xTjwZgeugzb6ac9k7fVveoJkkER6NG5W/vizNlUHSWdFAKy0kSz11g==", "GtB5unQeau/QPTip/Q27b9g0ut5avFacTLsM/rnUZfzslM9LU0Wnk5CdYAr4rzQiSk6gIy74gOOmiennFu4jIQ==");
@@ -19,6 +19,9 @@ namespace LibraryUnitTest
 
             // Delete the user
             UsersData.DeleteUser(userID);
+
+            // Check if the user was added
+            Assert.IsNull(UsersData.GetUserByEmail("test1@gmail.com"));
         }
 
         [TestMethod]
