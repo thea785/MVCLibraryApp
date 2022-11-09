baf05c8de76781d3701432539cff0233a09c5b2b
thea785 <theawallnc@gmail.com>
2022-11-09 09:55:13 -0500

    Added book data tests

--- LibraryUnitTest/BooksDataTest.cs
+++ LibraryUnitTest/BooksDataTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LibraryData;
+using System.Linq;
 
 namespace LibraryUnitTest
 {
@@ -9,7 +10,16 @@ namespace LibraryUnitTest
         [TestMethod]
         public void CreateBookTest()
         {
-            
+            int bookID = BooksData.CreateBook("1TestTitle", "1TestAuthor");
+            Assert.IsTrue(BooksData.GetBooks().Any(b => b.Title == "1TestTitle"));
+            BooksData.DeleteBook(bookID);
+            Assert.IsFalse(BooksData.GetBooks().Any(b => b.Title == "1TestTitle"));
+        }
+
+        [TestMethod]
+        public void CheckoutBookTest()
+        {
+            int bookID = BooksData.CreateBook("2TestTitle", "2TestAuthor");
         }
     }
 }
