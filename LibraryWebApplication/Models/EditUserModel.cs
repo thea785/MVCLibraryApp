a9b91f7f568a3bc0024af7983ba5d94049d37214
thea785 <theawallnc@gmail.com>
2022-11-11 10:29:10 -0500

    #5 Added edit user view and model

---
+++ LibraryWebApplication/Models/EditUserModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryWebApplication.Models
+{
+    public class EditUserModel
+    {
+        [Required]
+        [Range(2, 4)]
+        public int RoleID { get; set; }
+        [Required]
+        [EmailAddress]
+        [Remote(action: "VerifyEmail", controller: "Dashboard")]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
