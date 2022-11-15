a5cee0323d07732dea1aa13d71d0e6bf3a65ec63
thea785 <theawallnc@gmail.com>
2022-11-15 11:46:27 -0500

    Closes #13, Added password regex and string length validation

--- LibraryWebApplication/Models/RegisterModel.cs
+++ LibraryWebApplication/Models/RegisterModel.cs
@@ -15,6 +15,8 @@ namespace LibraryWebApplication.Models
         [Required]
         public string LastName { get; set; }
         [Required]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Field must be atleast 8 characters")]
+        [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W_]).{6,}$", ErrorMessage = "At least one uppercase letter, 1 number and 1 special character.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
